e51f7175e036c358ba59ee272a21f20ef98fa435
Gustavo Noronha Silva <gustavo.noronha@collabora.com>
2022-03-01 11:17:12 -0300

    wip

--- src/bin/import-results.rs
+++ src/bin/import-results.rs
@@ -1,8 +1,10 @@
 use anyhow::{bail, Result};
+use glob::glob;
 use reqwest::blocking as reqwest;
-use std::fs::File;
+use std::fs;
 use std::io::Write;
-use std::path::PathBuf;
+use std::ops::Add;
+use std::path::{PathBuf, Path};
 
 static ALLOWED_PREFIXES: &[&str] = &[
     "https://github.com/kov/iocost-benchmarks/files/",
@@ -60,44 +62,91 @@ fn get_urls() -> Result<Vec<String>> {
 }
 
 fn download_url(url: &str) -> Result<String> {
+    println!("download_url: {}", url);
     let response = reqwest::get(url)?;
 
-    let contents = response.text()?;
+    let contents = response.bytes()?;
 
     // Use md5sum of the data as filename, we only care about exact duplicates.
-    let tmpdir = tempfile::Builder::new().prefix("iocost-benchmark").tempdir()?;
+    //let tmpdir = tempfile::Builder::new().prefix("iocost-benchmark").tempdir()?;
     let filename = format!("result-{:x}.json.gz", md5::compute(&contents));
 
-    let path = tmpdir.path().join(&filename);
+    let path = PathBuf::from(&filename); // tmpdir.path().join(&filename);
 
-    let mut file = File::create(filename)?;
-    file.write_all(contents.as_bytes())?;
+    let mut file = fs::File::create(&path)?;
+    file.write_all(&contents)?;
 
     Ok(path.to_string_lossy().to_string())
 }
 
 fn get_normalized_model_name(filename: &str) -> Result<String> {
-    let workspace = PathBuf::from(std::env::var("GITHUB_WORKSPACE")?);
-    let output = std::process::Command::new(workspace.join("./resctl-demo/target/release/resctl-bench"))
+    let output = std::process::Command::new("./resctl-demo/target/release/resctl-bench")
         .args(&["--result", filename, "info"])
         .output()?;
-    Ok(String::from_utf8(output.stdout)?)
+
+    if !output.stderr.is_empty() {
+        panic!("{}", String::from_utf8(output.stderr)?);
+    }
+
+    let output = String::from_utf8(output.stdout)?;
+    Ok(
+        output.split_once('\n')
+            .unwrap().0
+            .split_once(": ")
+            .unwrap().1
+            .split_whitespace()
+            .collect::<Vec<&str>>()
+            .join("_")
+    )
 }
 
 fn get_summary(filename: &str) -> Result<String> {
     Ok(String::new())
 }
 
+fn merge_results_in_dir(path: &Path) -> Result<()> {
+    let results = glob(&format!("{}/result-*.json.gz", path.to_string_lossy().to_string()))
+        .unwrap()
+        .into_iter()
+        .flatten()
+        .map(|p| p.to_string_lossy().to_string());
+
+    let merged_path = path.join("merged-results.json.gz").to_string_lossy().to_string();
+    let mut arguments = vec!["--result".to_string(), merged_path, "merge".to_string()];
+    arguments.extend(results);
+
+    let output = std::process::Command::new("./resctl-demo/target/release/resctl-bench")
+        .args(arguments.as_slice())
+        .output()?;
+
+    if !output.stderr.is_empty() {
+        panic!("{}", String::from_utf8(output.stderr)?);
+    }
+
+    let output = String::from_utf8(output.stdout)?;
+    println!("{}", output);
+    Ok(())
+}
+
 fn main() -> Result<()> {
-    let token = std::env::var("GITHUB_TOKEN")?;
-    octocrab::initialise(octocrab::Octocrab::builder().personal_token(token))?;
+    /*let token = std::env::var("GITHUB_TOKEN")?;
+    octocrab::initialise(octocrab::Octocrab::builder().personal_token(token))?;*/
 
     let urls = get_urls()?;
     for url in urls {
         let filename = download_url(&url)?;
         let model_name = get_normalized_model_name(&filename)?;
+
         let summary = get_summary(&filename)?;
+
         println!("filename: {filename} model: {model_name}\nsummary: {summary}");
+
+        let model_directory = PathBuf::from(format!("database/{}", model_name));
+        fs::create_dir(&model_directory).ok();
+
+        fs::rename(&filename, model_directory.join(&filename))?;
+
+        merge_results_in_dir(model_directory.as_path())?;
     }
 /*
     let issue = octocrab::instance()
